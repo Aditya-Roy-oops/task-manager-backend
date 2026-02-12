f5bfde86a0a1bc889116f439b46bec1d61b19690
Aditya Sharma <asrpcp11@gmail.com>
2026-02-13 02:31:02 +0530

    Update task.controller.js

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -2,41 +2,67 @@ const { v4: uuidv4 } = require("uuid");
 const taskModel = require("../models/task.model");
 const logAction = require("../utils/logger");
 
-exports.createTask = async (req,res,next)=>{
- try{
-   const id = uuidv4();
-   const {title,description,status="pending"} = req.body;
-
-   await taskModel.create(id,req.user.id,title,description,status);
-   await logAction(req.user.id,"TASK_CREATED");
-
-   res.json({message:"Task created",id});
- }catch(err){next(err);}
+// helper to safely get user id
+const getUserId = (req) => {
+  if (!req.user || !req.user.id) {
+    throw new Error("User not authenticated properly");
+  }
+  return req.user.id;
 };
 
-exports.getTasks = async (req,res,next)=>{
- try{
-   const [rows] = await taskModel.findAllByUser(req.user.id);
-   res.json(rows);
- }catch(err){next(err);}
+
+exports.createTask = async (req, res, next) => {
+  try {
+    const userId = getUserId(req);
+    const id = uuidv4();
+    const { title, description, status = "pending" } = req.body;
+
+    await taskModel.create(id, userId, title, description, status);
+    await logAction(userId, "TASK_CREATED");
+
+    res.json({ message: "Task created", id });
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.updateTask = async (req,res,next)=>{
- try{
-   const {title,description,status} = req.body;
 
-   await taskModel.update(req.params.id,req.user.id,title,description,status);
-   await logAction(req.user.id,"TASK_UPDATED");
+exports.getTasks = async (req, res, next) => {
+  try {
+    const userId = getUserId(req);
+    const [rows] = await taskModel.findAllByUser(userId);
 
-   res.json({message:"Task updated"});
- }catch(err){next(err);}
+    res.json(rows);
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.deleteTask = async (req,res,next)=>{
- try{
-   await taskModel.remove(req.params.id,req.user.id);
-   await logAction(req.user.id,"TASK_DELETED");
 
-   res.json({message:"Task deleted"});
- }catch(err){next(err);}
+exports.updateTask = async (req, res, next) => {
+  try {
+    const userId = getUserId(req);
+    const { title, description, status } = req.body;
+
+    await taskModel.update(req.params.id, userId, title, description, status);
+    await logAction(userId, "TASK_UPDATED");
+
+    res.json({ message: "Task updated" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+exports.deleteTask = async (req, res, next) => {
+  try {
+    const userId = getUserId(req);
+
+    await taskModel.remove(req.params.id, userId);
+    await logAction(userId, "TASK_DELETED");
+
+    res.json({ message: "Task deleted" });
+  } catch (err) {
+    next(err);
+  }
 };
